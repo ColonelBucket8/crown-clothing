d18a4ab3ca255b1b066f345ab435b5cf3293156f
ColonelBucket8 <freedomterran89@gmail.com>
2022-01-31 10:47:57 +0800

    added redux persist to cartItems

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
