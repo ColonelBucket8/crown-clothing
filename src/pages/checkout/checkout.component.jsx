3feebb3df76b7ff84dece84ef171224506e01a05
Aimi Fadhil <59568628+ColonelBucket8@users.noreply.github.com>
2022-02-08 16:07:55 +0800

    refactored checkout to useSelector:

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
+import { useSelector } from "react-redux";
 import {
   selectCartItems,
   selectCartTotal,
@@ -15,43 +14,42 @@ import {
   TestWarningContainer,
 } from "./checkout.styles";
 
-const CheckoutPage = ({ cartItems, total }) => (
-  <CheckoutPageContainer>
-    <CheckoutHeaderContainer>
-      <HeaderBlockContainer>
-        <span>Product</span>
-      </HeaderBlockContainer>
-      <HeaderBlockContainer>
-        <span>Description</span>
-      </HeaderBlockContainer>
-      <HeaderBlockContainer>
-        <span>Quantity</span>
-      </HeaderBlockContainer>
-      <HeaderBlockContainer>
-        <span>Price</span>
-      </HeaderBlockContainer>
-      <HeaderBlockContainer>
-        <span>remove</span>
-      </HeaderBlockContainer>
-    </CheckoutHeaderContainer>
-    {cartItems.map((cartItem) => (
-      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
-    ))}
-    <TotalContainer>
-      <span>TOTAL: ${total}</span>
-    </TotalContainer>
-    <TestWarningContainer>
-      *Please use the following test credit card for payments*
-      <br />
-      4242 4242 4242 4242 - Exp: 01/23 - CVV: 123
-    </TestWarningContainer>
-    <StripeCheckoutButton />
-  </CheckoutPageContainer>
-);
+const CheckoutPage = () => {
+  const cartItems = useSelector(selectCartItems);
+  const total = useSelector(selectCartTotal);
+  return (
+    <CheckoutPageContainer>
+      <CheckoutHeaderContainer>
+        <HeaderBlockContainer>
+          <span>Product</span>
+        </HeaderBlockContainer>
+        <HeaderBlockContainer>
+          <span>Description</span>
+        </HeaderBlockContainer>
+        <HeaderBlockContainer>
+          <span>Quantity</span>
+        </HeaderBlockContainer>
+        <HeaderBlockContainer>
+          <span>Price</span>
+        </HeaderBlockContainer>
+        <HeaderBlockContainer>
+          <span>remove</span>
+        </HeaderBlockContainer>
+      </CheckoutHeaderContainer>
+      {cartItems.map((cartItem) => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
+      <TotalContainer>
+        <span>TOTAL: ${total}</span>
+      </TotalContainer>
+      <TestWarningContainer>
+        *Please use the following test credit card for payments*
+        <br />
+        4242 4242 4242 4242 - Exp: 01/23 - CVV: 123
+      </TestWarningContainer>
+      <StripeCheckoutButton />
+    </CheckoutPageContainer>
+  );
+};
 
-const mapStateToProps = createStructuredSelector({
-  cartItems: selectCartItems,
-  total: selectCartTotal,
-});
-
-export default connect(mapStateToProps)(CheckoutPage);
+export default CheckoutPage;
