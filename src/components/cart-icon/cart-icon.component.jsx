f78ab2e6bac4cf7b33c74d91ec190ae1ff7b6f20
Aimi Fadhil <59568628+ColonelBucket8@users.noreply.github.com>
2022-02-08 15:56:18 +0800

    refactored collections-overview to useSelector

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { createStructuredSelector } from "reselect";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 import {
